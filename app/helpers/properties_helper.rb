7ba75bbebb7617105348e0ecfb1eb8413dbea714
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-09-27 21:54:20 +0200

    Add sidebar
    - Helpers properties for images
    
    - Modify the side dashboard present if logged

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,7 +1,12 @@
 module PropertiesHelper
 
   def property_thumbnail property
-    img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+    img = property.photo.present? ? property.photo.thumb.url : ""
     image_tag img, class: "property-thumb"
   end
+
+  def property_photo_url property
+    img = property.photo.present? ? property.photo.url : asset_url("placeholder.png")
+  end
+
 end
