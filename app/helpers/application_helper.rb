dddb04123b2390ca2d20c3870f28cf79fd334359
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-09-30 14:15:50 +0200

    Edit account and add picture agent
    - Add the profile picture to the agent
    
    - Add the settings to redirect edit accoount on dashboard
    
    -Add helpers Profil picture and application_controller
    
    -Generate new uploader for agents
    
    -Modify edit account gorm group

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def profile_picture account
+    thumb = account.image.present? ? account.image.thumb.url : ""
+    image_tag thumb, class: "profile-pic img-circle"
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -4,15 +4,11 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
-  mount_uploader :image, PhotoUploader
+  mount_uploader :image, ProfilePictureUploader
 
   has_many :properties
 
   def full_name
     "#{first_name} #{last_name}"
-  end
-
-  def company
-    "test"
   end
 end
