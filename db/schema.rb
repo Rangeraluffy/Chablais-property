67992fac784296a3fb90e68141c77d05c8a52029
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-10-05 15:31:53 +0200

    Add details for agent to account edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_130221) do
+ActiveRecord::Schema.define(version: 2021_10_05_133015) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_130221) do
     t.string "company"
     t.string "telephone"
     t.boolean "admin", default: false
+    t.text "details"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
