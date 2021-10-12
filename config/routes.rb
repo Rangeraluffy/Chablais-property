97ff7f276ef1ed1d71780f82d6aec8f8934f6d6f
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-10-12 22:15:04 +0200

    Defines router scope for selecting locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  get 'contact/contact'
-  get 'agencies/thonon'
-  get 'agencies/douvaine'
-  get 'agencies/sciez'
-  get 'information/about'
-  devise_for :accounts
-  resources :posts
-  resources :properties
-
-  get "/blog" => "posts#latest", as: :blog
-
-  #admin routes
-  get "/accounts" => 'admin#accounts', as: :accounts
-
-  get "/dashboard" => 'dashboard#index', as: :dashboard
-  get "/profile/:id" => 'dashboard#profile', as: :profile
-
   root to: 'public#main'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope '/:locale' do
+    get 'contact/contact'
+    get 'agencies/thonon'
+    get 'agencies/douvaine'
+    get 'agencies/sciez'
+    get 'information/about'
+    devise_for :accounts
+    resources :posts
+    resources :properties
+
+    get "/blog" => "posts#latest", as: :blog
+
+    #admin routes
+    get "/accounts" => 'admin#accounts', as: :accounts
+
+    get "/dashboard" => 'dashboard#index', as: :dashboard
+    get "/profile/:id" => 'dashboard#profile', as: :profile
+
+    root to: 'public#main'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
