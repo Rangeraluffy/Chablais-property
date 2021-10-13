90d6d6ad1b2a0cb2b082274d8add675c147b6978
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-10-13 14:04:16 +0200

    Update all views
    - HTML , CSS remove some elements for esthetic

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,4 +1,10 @@
 class InformationController < ApplicationController
   def about
+      if account_signed_in?
+        path = current_account.admin? ? accounts_path : dashboard_path
+      end
+
+      @properties = Property.latest
+      @posts = Post.latest
   end
 end
