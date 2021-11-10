a1ecab203be2f07c0680913312d259ab77c3334d
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-11-10 13:32:49 +0100

    Create first Test Scrypt

--- test/system/using_capybara_test.rb
+++ test/system/using_capybara_test.rb
@@ -8,7 +8,7 @@ class CapybaraTest < ApplicationSystemTestCase
     sleep(5.seconds)
     fill_in('q', with: 'Ruby on Rails')
     sleep(5.seconds)
-    click_on('Search', match: :first)
+    click_on('Recherche', match: :first)
     click_on('Ruby on Rails', match: :first)
     sleep(5.seconds)
     click_on('Guides')
