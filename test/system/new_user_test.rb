aefa4d6e35c1d91909395b88f33674dee5979caf
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-11-10 17:04:53 +0100

    Automating testing User Log In
    - User log In and use the dashboard and update fill in account

--- test/system/new_user_test.rb
+++ test/system/new_user_test.rb
@@ -10,4 +10,22 @@ class NewUserTest < ApplicationSystemTestCase
     fill_in 'Password confirmation', with: '123456'
     click_button 'Sign up', match: :first
   end
+
+  test "New user log In test dashboard and Edit Account" do
+    visit new_account_registration_path
+    fill_in 'First name', with: 'Automating'
+    fill_in 'Last name', with: 'Testing'
+    fill_in 'Email', with: 'automating74@testing.fr'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_button 'Sign up', match: :first
+    click_on('Dashboard', match: :first)
+    click_on('My Profile', match: :first)
+    click_on('Edit my Account', match: :first)
+    fill_in 'First name', with: 'Valentin'
+    fill_in 'Last name', with: 'Smile'
+    fill_in 'Telephone', with: '06.50.60.78.52'
+    fill_in 'Current password', with: '123456'
+    click_on('Update', match: :first)
+  end
 end
